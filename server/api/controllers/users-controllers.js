8f2a8b41d2b65487544e105bb64a21d00f9f2f56
annanakashimo <anna.nakashimo@gmail.com>
2021-10-10 01:04:43 -0700

    Added login and delete user method.

--- server/api/controllers/users-controllers.js
+++ server/api/controllers/users-controllers.js
@@ -1,38 +1,133 @@
 // Ensure that only the right data makes it into the database
-const { validationResult } = require('express-validator'); // Ensure that only the right data makes it into the database
+const { validationResult } = require('express-validator');
+
+// Handles http error
+const HttpError = require('../../models/http-error');
 
 // This is used for password encryption
 const bcrypt = require('bcrypt');
-const Users = require('../../models/users');
 
+const Users = require('../../models/user');
+
+/* Signup method */
 const addUsers = async (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({
-        error: err
-      });
-    } else {
-      const addedUser = new Users({
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
+  }
 
-      const doAdd = async (req, res, next) => {
-        console.log(addedUser);
-        try {
-          await addedUser.save();
-        } catch(err) {
-          console.log("Failed");
-          return next(err);
-        }
-      };
+  let existingUser;
+  try {
+    existingUser = await Users.findOne({ email : req.body.email });
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.", 
+      500
+    );
+    return next(error);
+  }
 
-      doAdd();
+  if (existingUser) {
+    const error = new HttpError(
+      "You already have an account, please login instead.",
+      422
+    );
+    return next(error);
+  }
 
-      res.status(201).json({user: addedUser.toObject({ getters: true })});
-    }
-  })
-}
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(req.body.password, 12);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not create user, please try again", 
+      500
+    );
+    return next(error);
+  }
+
+  const addedUser = new Users({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword, 
+    tasks: [],
+  });
+
+  try {
+    await addedUser.save();
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again.", 500);
+    return next(error);
+  }
+
+  res.status(201).json({user: addedUser.toObject({ getters: true })});
+};
+
+/* Login method */
+const userLogin = async (req, res, next) => {
+  // Check if the user exits
+  let exitingEmail;
+  try {
+    exitingEmail = await Users.findOne({ email: req.body.email });
+  } catch(err) {
+    const error = new HttpError(
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!exitingEmail) {
+    const error = new HttpError(
+      "Invalid emial or password, please try iy again.",
+      401
+    );
+    return next(error);s
+  }
+
+  // Check if the password is valid
+  let isValidPassword;
+  try {
+    isValidPassword = await bcrypt.compare(req.body.password, exitingEmail.password);
+  } catch(err) {
+    const error = new HttpError(
+      "Failed logging you in, please try again.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      "Invalid emial or password, please try iy again.",
+      401
+    );
+    return next(error);
+  }
+
+  res.json({ message: "Logged in!" });
+};
+
+/* Delete accounts method */
+const deleteUsers = async (req, res, next) => {
+  console.log("executing deleteUsers function");
+  let existingUserId;
+  try {
+    existingUserId = await Users.findByIdAndRemove({ _id: req.params.userId });
+  } catch(err) {
+    const error = new HttpError(
+      "Failed deleting the user",
+      500
+    );
+    return next(error);
+  }
+
+  res.json({ message: "User deleted." });
+};
+
 
 exports.addUsers = addUsers;
+exports.userLogin = userLogin;
+exports.deleteUsers = deleteUsers;

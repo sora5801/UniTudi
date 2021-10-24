856423eb2b0fe5d595743d1532bab948bb838b22
Anna Nakashimo <annanakashimo@Annas-Air.hsd1.ca.comcast.net>
2021-10-23 17:29:16 -0700

    Recommit user schema changes lost in rebase

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,17 @@
+
+const { Int32, Double } = require('bson');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const usersSchema = new Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true}, 
-    password: {type: String, required: true}, 
+const userSchema = new Schema({
+    name: { type: String, required: true} ,
+    email: { type: String, required: true}, 
+    password: { type: String, required: true},
+    major: { type: String, required: false},
+    graduationDate: { type: Date, required: false},
+    avaliableHours: { type: Number, required: false},
     tasks: [{type: mongoose.Types.ObjectId, required: true, ref: 'task'}]
 });
 
-module.exports = mongoose.model('User', usersSchema);
+module.exports = mongoose.model('User', userSchema);
